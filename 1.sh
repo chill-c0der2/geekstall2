44e8b7188a90675b8c87e9dc0ab2939c408ccb97
chill-c0der <99861040+chill-c0der2@users.noreply.github.com>
2022-05-25 22:24:04 -0700

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,46 @@
+dependencies() {
+	echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing required packages..."
+
+    if [[ -d "/data/data/com.termux/files/home" ]]; then
+        if [[ `command -v proot` ]]; then
+            printf ''
+        else
+			echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing package : ${ORANGE}proot${CYAN}"${WHITE}
+            pkg install proot resolv-conf -y
+        fi
+
+        if [[ `command -v tput` ]]; then
+            printf ''
+        else
+			echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing package : ${ORANGE}ncurses-utils${CYAN}"${WHITE}
+            pkg install ncurses-utils -y
+        fi
+
+    fi
+
+	if [[ `command -v php` && `command -v wget` && `command -v curl` && `command -v unzip` ]]; then
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${GREEN} Packages already installed."
+	else
+		pkgs=(figlet git python3 ruby)
+		for pkg in "${pkgs[@]}"; do
+			type -p "$pkg" &>/dev/null || {
+				echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing package : ${ORANGE}$pkg${CYAN}"${WHITE}
+				if [[ `command -v pkg` ]]; then
+					pkg install "$pkg" -y
+				elif [[ `command -v apt` ]]; then
+					apt install "$pkg" -y
+				elif [[ `command -v apt-get` ]]; then
+					apt-get install "$pkg" -y
+				elif [[ `command -v pacman` ]]; then
+					sudo pacman -S "$pkg" --noconfirm
+				elif [[ `command -v dnf` ]]; then
+					sudo dnf -y install "$pkg"
+				else
+					echo -e "\n${RED}[${WHITE}!${RED}]${RED} Unsupported package manager, Install packages manually."
+					{ reset_color; exit 1; }
+				fi
+			}
+		done
+	fi
+
+}
