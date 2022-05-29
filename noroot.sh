9493e475a685a8b6718c79dfd45a64369d74a52f
chill-c0der <99861040+chill-c0der2@users.noreply.github.com>
2022-05-29 13:26:21 -0700

    Update noroot.sh

--- noroot.sh
+++ noroot.sh
@@ -1,14 +1,3 @@
-#!/usr/bin/env bash
-packages=("figlet" "ruby" "python3" "git")
-
-for pkg in ${packages[@]}; do
-
-    is_pkg_installed=$(dpkg-query -W --showformat='${Status}\n' ${pkg} | grep "install ok installed")
-
-    if [ "${is_pkg_installed}" == "install ok installed" ]; then
-        echo ${pkg} is installed.
-    fi
-done
 clear
 echo DETECTING.
 sleep 1
@@ -26,54 +15,40 @@ echo -e "\e[1;31m Coded By Zaid Yasin \e[0m"
 echo -e "\e[1;31m Email: zaidoaidoalfredo@gmail.com \e[0m"
 sleep 2
 PS3='root@localstall '
-options=("nmap" "aircrack" "zphisher" "routersploit" "DDos") 
+options=("nmap" "aircrack" "zphisher" "routersploit" "DDos" "airgeddon") 
 select opt in "${options[@]}" 
 do
     case $opt in
         "nmap")
-            apt install nmap| lolcat
-            sleep 2 | lolcat
-            nmap | lolcat
-            sleep 5 | lolcat
-            figlet installed | lolcat
-            sleep 2 | lolcat
-            clear
+            apt install nmap
             ;;
         "aircrack")
-            cd ..
             apt install aircrack-ng | lolcat
-            sleep 2 | lolcat
-            aircrack-ng | lolcat
-            sleep 5 | lolcat
-            figlet installed | lolcat
-            sleep 2 | lolcat
             clear
             ;;
             "DDos")
             cd ..
             git clone https://github.com/palahsu/DDoS-Ripper
-            cd DDoS-Ripper
-            python3 DRipper.py
-            sleep 2
+            cd geekstall2
             clear
             ;;
         "zphisher")
             cd ..
-            git clone https://github.com/htr-tech/zphisher.git | lolcat
-            cd zphisher
-            bash zphisher.sh
-            sleep 2
-            figlet Press Enter
-            sleep 2
+            git clone https://github.com/htr-tech/zphisher.git
+            cd geekstall2
+            clear
             ;;
         "routersploit")
+            apt install python3
             cd ..
             git clone https://github.com/threat9/routersploit.git
-            cd routersploit
-            python3 rsf.py
+            cd geekstall2
             clear
-            sleep 2
             ;;
+            "airgeddon")
+            cd ..
+            git clone https://github.com/v1s1t0r1sh3r3/airgeddon
+            cd geekstall2
         *) echo "invalid!! $REPLY";;
     esac
 done | lolcat
