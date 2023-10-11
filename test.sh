2228e8ba0f7e88fff04fdee214228c2f3ec51e16
chill-c0der <99861040+chill-c0der2@users.noreply.github.com>
2023-10-11 16:19:46 -0700

    commit all

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
-if ! which ruby > /dev/null; then
-   echo -e  "${GREEN}Install? (y/n) \c"
+if ! which python3 > /dev/null; then
+   echo -e "Command not found! Install? (y/n) \c"
    read
    if "$REPLY" = "y"; then
-      apt-get install ruby
-   fi 
+      apt-get install python3
+   fi
+fi
