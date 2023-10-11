8ee201cbbf00cf37c07eb3a3bcd4c0303d188b5f
chill-c0der <99861040+chill-c0der2@users.noreply.github.com>
2023-10-11 16:23:42 -0700

    commit all

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-if [ $(dpkg-query -W -f='${Status}' nano 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if [ $(dpkg-query -W -f='${Status}' python3 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  apt-get install nano;
+  apt-get install python3;
 fi
