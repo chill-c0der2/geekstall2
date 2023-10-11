12a8700b29be1880b89c5ebf0b0c75001be4aa21
chill-c0der <99861040+chill-c0der2@users.noreply.github.com>
2023-10-11 16:25:39 -0700

    commit all

--- test.sh
+++ test.sh
@@ -2,3 +2,6 @@ if [ $(dpkg-query -W -f='${Status}' python3 2>/dev/null | grep -c "ok installed"
 then
   apt-get install python3;
 fi
+if [[ $(command -v python3) ]]; then
+    echo "python3 already installed"
+fi
