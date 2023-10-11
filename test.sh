67d92f0ee04c5be58d7b68b21f0f53a1af996db9
chill-c0der <99861040+chill-c0der2@users.noreply.github.com>
2023-10-11 16:17:54 -0700

    commit all

--- test.sh
+++ test.sh
@@ -1,26 +1,6 @@
-if ! which python3 > /dev/null; then
-   echo -e  "${GREEN}Install? (y/n) \c"
-   read
-   if "$REPLY" = "y"; then
-      apt-get install python3
-   fi
-else
 if ! which ruby > /dev/null; then
    echo -e  "${GREEN}Install? (y/n) \c"
    read
    if "$REPLY" = "y"; then
       apt-get install ruby
-   fi
-else
-if ! which pip > /dev/null; then
-   echo -e  "${GREEN}Install? (y/n) \c"
-   read
-   if "$REPLY" = "y"; then
-      apt-get install python3-pip
-   fi
-   else 
-   exit
-   ~
-   ~
-   ~
-   ~
+   fi 
