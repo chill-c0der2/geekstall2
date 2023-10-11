d6552b7d8de81970a58820bfde2c064119c8e340
chill-c0der <99861040+chill-c0der2@users.noreply.github.com>
2023-10-11 15:58:33 -0700

    commit all

--- noroot.sh
+++ noroot.sh
@@ -1,3 +1,35 @@
+if [[ $(command -v python3) ]]; then
+    echo "python3 is already installed"
+fi
+if ! which python3 > /dev/null; then
+   echo -e  "${GREEN}Install? (y/n) \c"
+   read
+   if "$REPLY" = "y"; then
+      apt-get install python3
+   fi
+
+if [[ $(command -v ruby) ]]; then
+    echo "ruby is already installed"
+fi
+if ! which ruby > /dev/null; then
+   echo -e  "${GREEN}Install? (y/n) \c"
+   read
+   if "$REPLY" = "y"; then
+      apt-get install ruby
+   fi
+fi
+gem install lolcat
+if [[ $(command -v pip) ]]; then
+    echo "python3 is already installed"
+fi
+if ! which pip > /dev/null; then
+   echo -e  "${GREEN}Install? (y/n) \c"
+   read
+   if "$REPLY" = "y"; then
+      apt-get install python3-pip
+   fi
+fi
+sleep 10
 clear
 echo DETECTING.
 sleep 1
