9009a97141d1c5191877be8bae3149817ef4bd72
chill-c0der <99861040+chill-c0der2@users.noreply.github.com>
2023-10-12 09:39:40 -0700

    commit all

--- test.sh
+++ test.sh
@@ -6,13 +6,13 @@ if [[ $(command -v python3) ]]; then
    GREEN='\033[0;92m'
     echo -e "${GREEN}python3 already installed"
 fi
-
-if [ $(dpkg-query -W -f='${Status}' python3 2>/dev/null | grep -c "ok installed") -eq 0 ];
+sleep 2
+if [ $(dpkg-query -W -f='${Status}' pip 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-  apt-get install python3;
+  apt-get install python3-pip;
 fi
-if [[ $(command -v python3) ]]; then
+if [[ $(command -v pip) ]]; then
    GREEN='\033[0;92m'
-    echo -e "${GREEN}python3 already installed"
+    echo -e "${GREEN}pip is already installed"
 fi
 
